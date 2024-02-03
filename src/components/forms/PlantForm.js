115a2d92591dbd302978d595d6cdd7e1f2572ace
Charles Knepshield <aknepshield@gmail.com>
2024-02-03 12:51:29 -0600

    no more errors but getPlantInfo still undefined

--- src/components/forms/PlantForm.js
+++ src/components/forms/PlantForm.js
@@ -5,39 +5,31 @@ import { useNavigate, useParams } from "react-router-dom";
 
 export const PlantForm = ({ currentUser }) => {
   const { plantId } = useParams();
-  // Change all 3 to set states
-  const [rooms, setRooms] = useState("");
-  const [waterLevels, setWaterLevels] = useState("");
-  const [lightNeeded, setLightNeeded] = useState("");
+  const [rooms, setRooms] = useState([]);
 
   const [selectedRoom, setSelectedRoom] = useState("");
-  const [selectedWaterLevel, setSelectedWaterLevel] = useState("");
-  const [selectedSunNeeded, setSelectedSunNeeded] = useState("");
-  const [currentPlantInfo, setCurrentPlantInfo] = useState({});
+  const [currentPlantInfo, setCurrentPlantInfo] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    getPlantInfo(plantId).then((plantInfo) => {
-      setCurrentPlantInfo(plantInfo);
-      setSelectedRoom(plantInfo.room.id); // returns undefined and when commented out undefined cascades down the list
-      setSelectedWaterLevel(plantInfo.waterLevel);
-      setSelectedSunNeeded(plantInfo.lightNeeded);
+    getPlantInfo(plantId).then((currentPlantInfo) => {
+      if (
+        currentPlantInfo &&
+        currentPlantInfo.room &&
+        currentPlantInfo.room.roooms
+      ) {
+        setRooms(Object.values(currentPlantInfo.room.rooms));
+        setSelectedRoom(currentPlantInfo.room.roomName);
+        setCurrentPlantInfo(currentPlantInfo);
+      }
     });
   }, [plantId, currentUser]);
 
-  // ^ This whole useEffect is greyed out in dev tools
-
   const handleRoomChange = (event) => {
-    setSelectedRoom(event.target.value);
-  };
-
-  const handleWaterLevelChange = (event) => {
-    setSelectedWaterLevel(event.target.value);
-  };
-
-  const handleSunNeededChange = (event) => {
-    setSelectedSunNeeded(event.target.value);
+    const stateCopy = { ...rooms };
+    stateCopy[event.target.name] = event.target.value;
+    setSelectedRoom(stateCopy);
   };
 
   const handleSave = (event) => {
@@ -48,8 +40,8 @@ export const PlantForm = ({ currentUser }) => {
       userId: currentUser.id,
       roomId: selectedRoom,
       type: currentPlantInfo.type,
-      waterLevel: selectedWaterLevel,
-      lightNeeded: selectedSunNeeded,
+      waterLevel: currentPlantInfo.waterLevel,
+      lightNeeded: currentPlantInfo.selectedSunNeeded,
       datePlanted: currentPlantInfo.datePlanted,
     };
 
@@ -79,48 +71,11 @@ export const PlantForm = ({ currentUser }) => {
         </div>
       </fieldset>
 
-      <fieldset>
-        <div className="form-group">
-          <label>Watering Levels:</label>
-          <select
-            className="form-control"
-            value={selectedWaterLevel}
-            onChange={handleWaterLevelChange}
-          >
-            <option value="">Select Water Level</option>
-            {waterLevels.map((waterLevel) => (
-              <option key={waterLevel} value={waterLevel}>
-                {waterLevel}
-              </option>
-            ))}
-          </select>
-        </div>
-      </fieldset>
-
-      <fieldset>
-        <div className="form-group">
-          <label>Sunlight Needed:</label>
-          <select
-            className="form-control"
-            value={selectedSunNeeded}
-            onChange={handleSunNeededChange}
-          >
-            <option value="">Select Sunlight Needed</option>
-            {lightNeeded.map((lightNeeded) => (
-              <option key={lightNeeded} value={lightNeeded}>
-                {lightNeeded}
-              </option>
-            ))}
-          </select>
-        </div>
-      </fieldset>
-      <fieldset>
-        <div className="form-group">
-          <button className="form-btn" onClick={handleSave}>
-            Save
-          </button>
-        </div>
-      </fieldset>
+      <div className="form-group">
+        <button className="form-btn" onClick={handleSave}>
+          Save
+        </button>
+      </div>
     </form>
   );
 };
