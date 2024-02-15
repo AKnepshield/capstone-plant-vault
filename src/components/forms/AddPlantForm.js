4dca56b324e4f818dcd907c7b237bfe19346e5df
Charles Knepshield <aknepshield@gmail.com>
2024-02-15 14:03:25 -0600

    styling done

--- src/components/forms/AddPlantForm.js
+++ src/components/forms/AddPlantForm.js
@@ -9,6 +9,8 @@ export const AddPlantForm = ({ currentUser }) => {
   const [rooms, setRooms] = useState([]);
   const [newSelectedRoom, setNewSelectedRooom] = useState(0);
   const [plantAdded, setPlantAdded] = useState(false);
+  const [selectedWaterLevel, setSelectedWaterLevel] = useState("");
+  const [selectedLightNeeded, setSelectedLightNeeded] = useState("");
 
   useEffect(() => {
     getAllRooms().then((allRooms) => {
@@ -21,6 +23,18 @@ export const AddPlantForm = ({ currentUser }) => {
     setNewSelectedRooom(selectedRoomId);
   };
 
+  const waterLevelOptions = [
+    { id: 1, name: "Low", value: "Low" },
+    { id: 2, name: "Medium", value: "Medium" },
+    { id: 3, name: "High", value: "High" },
+  ];
+
+  const lightNeededOptions = [
+    { id: 1, name: "Low", value: "Low" },
+    { id: 2, name: "Medium", value: "Medium" },
+    { id: 3, name: "High", value: "High" },
+  ];
+
   const navigate = useNavigate();
 
   const handleAdd = (event) => {
@@ -31,8 +45,8 @@ export const AddPlantForm = ({ currentUser }) => {
         roomId: newSelectedRoom,
         type: plant.type,
         image: `/images/${plant.type}.jpg`,
-        waterLevel: plant.waterLevel,
-        lightNeeded: plant.lightNeeded,
+        waterLevel: selectedWaterLevel,
+        lightNeeded: selectedLightNeeded,
         datePlanted: new Date().toISOString().slice(0, 10),
       };
       addPlant(newPlant).then(() => {
@@ -49,13 +63,12 @@ export const AddPlantForm = ({ currentUser }) => {
   return (
     <div className="container">
       <div className="row justify-content-center align=items-center vh-100">
-        <div className="col-md-6">
+        <div className="col-md-8">
           <form>
-            <h2>Add Plant</h2>
+            <h1 style={{ fontSize: "24px" }}>Add Plant</h1>
             <div className="mb-3">
               <fieldset>
-                <div className="form-group">
-                  <label>Type</label>
+                <div key="form-group-key" className="form-group">
                   <input
                     type="text"
                     className="form-control"
@@ -69,43 +82,85 @@ export const AddPlantForm = ({ currentUser }) => {
                 </div>
               </fieldset>
             </div>
-            <fieldset>
-              <div className="mb-3">
-                <div className="form-group">
-                  <label>Water Level</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Low, Medium, High"
-                    onChange={(event) => {
-                      const plantCopy = { ...plant };
-                      plantCopy.waterLevel = event.target.value;
-                      setPlant(plantCopy);
-                    }}
-                  />
-                </div>
+
+            <div>
+              <label
+                className="form-label"
+                style={{ fontSize: "24px" }}
+                id="water-level-label"
+              >
+                Water Level
+              </label>
+              <div
+                className="btn-group"
+                role="group"
+                aria-label="Basic radio toggle button group"
+              >
+                {waterLevelOptions.map((waterLevelOption, index) => (
+                  <>
+                    {" "}
+                    <input
+                      type="radio"
+                      name="waterLevelOptions"
+                      id={`input-${waterLevelOption.name}-${index}`}
+                      value={waterLevelOption.name}
+                      key={`input-${waterLevelOption.name}-${index}-key`}
+                      autoComplete="off"
+                      checked={selectedWaterLevel === waterLevelOption.value}
+                      onChange={(event) => {
+                        console.log(event);
+                        setSelectedWaterLevel(event.target.value);
+                      }}
+                    />
+                    <label
+                      className="btn btn-outline-warning rounded-pill"
+                      key={`label-${waterLevelOption.name}-${index}-key`}
+                      id={`label-${waterLevelOption.name}-${index}`}
+                    >
+                      {waterLevelOption.name}
+                    </label>
+                  </>
+                ))}
               </div>
-            </fieldset>
-            <div className="mb-3">
-              <fieldset>
-                <div className="form-group">
-                  <label>Amount of sunlight</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Low, Medium, High"
-                    onChange={(event) => {
-                      const plantCopy = { ...plant };
-                      plantCopy.lightNeeded = event.target.value;
-                      setPlant(plantCopy);
-                    }}
-                  />
-                </div>
-              </fieldset>
+            </div>
+            <div>
+              <label className="form-label" style={{ fontSize: "24px" }}>
+                Amount of sunlight needed
+              </label>
+              <div
+                className="btn-group"
+                role="group"
+                aria-label="Basic radio toggle button group"
+              >
+                {lightNeededOptions.map((lightNeededOption, index) => (
+                  <>
+                    <input
+                      type="radio"
+                      className="btn-success"
+                      name={lightNeededOption.name}
+                      id={`input-${lightNeededOption.name}-${index}`}
+                      key={`input-${lightNeededOption.name}-${index}-key`}
+                      autoComplete="off"
+                      checked={selectedLightNeeded === lightNeededOption.value}
+                      onChange={() => {
+                        setSelectedLightNeeded(lightNeededOption.value);
+                      }}
+                    />
+                    <label
+                      value={lightNeededOption.value}
+                      className="btn btn-outline-warning rounded-pill"
+                      key={`label-${lightNeededOption.name}-${index}-key`}
+                      id={`label-${lightNeededOption.name}-${index}`}
+                    >
+                      {lightNeededOption.name}
+                    </label>
+                  </>
+                ))}
+              </div>
             </div>
             <fieldset>
               <div className="form-group">
-                <label>Room:</label>
+                <label style={{ fontSize: "24px" }}>Room:</label>
                 <select
                   className="form-control"
                   value={newSelectedRoom}
@@ -129,7 +184,7 @@ export const AddPlantForm = ({ currentUser }) => {
                   <div className="col">
                     <button
                       type="button"
-                      class="btn btn-outline-light"
+                      className="btn btn-outline-light"
                       onClick={handleAdd}
                       style={{ float: "left" }}
                     >
